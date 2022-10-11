4badf5ae886e2e8bab020d9170c03bd0ca0a15dc
ingeb0rga <104138696+ingeb0rga@users.noreply.github.com>
2022-10-11 16:05:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import sys
 
 if __name__ == "__main__":
     calc = Calculator()
-    arg1 = int(sys.argv[2])
-    arg2 = int(sys.argv[3])
-    if sys.argv[1] == "add":
-        result = calc.add(arg1, arg2)
-    elif sys.argv[1] == "sub":
-        result = calc.sub(arg1, arg2)
-    elif sys.argv[1] == "mul":
-        result = calc.mul(arg1, arg2)
-    elif sys.argv[1] == "div":
-        result = calc.div(arg1, arg2)
-    else:
-        print("Wrong command passed")
-        result = None
-    
+    arg1 = float(sys.argv[2])
+    arg2 = float(sys.argv[3])
+    match sys.argv[1]:
+        case "add":
+            result = calc.add(arg1, arg2)
+        case "sub":
+            result = calc.sub(arg1, arg2)
+        case "mul":
+            result = calc.mul(arg1, arg2)
+        case "div":
+            result = calc.div(arg1, arg2)
+        case _:
+            print("Wrong command passed")
+            result = None
+        
     print(f"Result: {result}")
